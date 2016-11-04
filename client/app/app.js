2fdfd3f016233597222734a057e6a5fc9d2e70dd
Agustin Pivetta <aguspivetta@gmail.com>
2016-11-04 16:31:29 -0300

    headers

--- client/app/app.js
+++ client/app/app.js
@@ -19,11 +19,25 @@ angular.module('app', [
     Common,
     Components
   ])
-  .config(($locationProvider) => {
+  .config(($locationProvider, $mdThemingProvider) => {
     "ngInject";
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue', {
+        'default': '700',
+        'hue-1': '100',
+        'hue-2': '600',
+        'hue-3': 'A100'
+      })
+      .accentPalette('green', {
+        'default': '600', 
+        'hue-1': '100', 
+        'hue-2': '600',
+        'hue-3': 'A100'
+      });
   })
 
 .component('app', AppComponent);
